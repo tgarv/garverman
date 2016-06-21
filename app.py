e770ea9c83f2742566f48e54c16d4e37700b87d8
Thomas Garver <tgarv@mit.edu>
2016-06-21 20:42:22 +0000

    Reponsive header and placeholders for main pages

--- app.py
+++ app.py
@@ -7,22 +7,38 @@ app = Flask(__name__)
 @app.route('/')
 def index():
     return render_template('index.html')
-    return 'Coming Soon!'
 
-# @app.route('/gallery')
-# def gallery():
-#     photos = get_photos()
-#     return render_template('gallery.html', photos=photos)
+@app.route('/v2')
+def index2():
+    return render_template('index2.html')
 
-def get_photos():
-    photo_urls = [] # TODO add metadata (title and caption) here
-    dbx = dropbox.Dropbox('WJAqPX-UcboAAAAAAABBl_HUSU6Tj1iwdWPpdMzZyO0z9768QnxRusoUZm80Kd10')
-    for entry in dbx.files_list_folder('').entries:
-        link = dbx.sharing_create_shared_link(entry.path_lower)
-        url = link.url.replace("dl=0", "dl=1")
-        photo_urls.append(url)
+@app.route('/rsvp')
+def rsvp():
+    return render_template('rsvp.html')
 
-    return photo_urls
+@app.route('/request_electronic_invitation')
+def request_electronic_invitation():
+    return render_template('request_electronic_invitation.html')
+
+@app.route('/our_story')
+def our_story():
+    return render_template('our_story.html')
+
+@app.route('/regrets')
+def regrets():
+    return render_template('regrets.html')
+
+@app.route('/gallery')
+def gallery():
+    return render_template('gallery.html')
+
+@app.route('/wedding_info')
+def wedding_info():
+    return render_template('base.html')
+
+@app.route('/lodging_info')
+def lodging_info():
+    return render_template('base.html')
 
 if __name__ == '__main__':
     port = int(os.environ.get('PORT', 5000))
