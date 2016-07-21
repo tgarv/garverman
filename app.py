7e06f5e66990265d501fb4bd71fd021ff2b84d7c
Thomas Garver <tgarver@wayfair.com>
2016-07-21 15:24:40 -0400

    Remove external css and js dependencies

--- app.py
+++ app.py
@@ -38,4 +38,5 @@ def lodging_info():
 
 if __name__ == '__main__':
     port = int(os.environ.get('PORT', 5000))
-    app.run(host='0.0.0.0', port=port)
+    app.run()
+    # app.run(host='0.0.0.0', port=port)
