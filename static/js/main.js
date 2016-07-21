8da55c81e56eac717964f9f78d2bcf4ed697c8fa
Thomas Garver <tgarv@mit.edu>
2016-07-21 14:50:15 +0000

    Add info sections on home page

--- static/js/main.js
+++ static/js/main.js
@@ -1,3 +1,6 @@
+/* global $ */
 $(document).ready(function() {
-
+    $('.js-expand-location-information').on('click', function() {
+        $('.js-location-information').toggleClass('hidden');
+    });
 });
