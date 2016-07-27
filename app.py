b6df1b42b145af678f82f8f113c5f127211e1ee5
Thomas Garver <tgarver@wayfair.com>
2016-07-27 17:14:01 -0400

    Add styles for more content sections

--- app.py
+++ app.py
@@ -3,38 +3,48 @@ import os
 import dropbox
 
 app = Flask(__name__)
+local_files = False
+
 
 @app.route('/')
 def index():
-    return render_template('index.html')
+    return render_template('index.html', local_files=local_files)
+
 
 @app.route('/rsvp')
 def rsvp():
-    return render_template('rsvp.html')
+    return render_template('rsvp.html', local_files=local_files)
+
 
 @app.route('/request_electronic_invitation')
 def request_electronic_invitation():
-    return render_template('request_electronic_invitation.html')
+    return render_template('request_electronic_invitation.html', local_files=local_files)
+
 
 @app.route('/our_story')
 def our_story():
-    return render_template('our_story.html')
+    return render_template('our_story.html', local_files=local_files)
+
 
 @app.route('/regrets')
 def regrets():
-    return render_template('regrets.html')
+    return render_template('regrets.html', local_files=local_files)
+
 
 @app.route('/gallery')
 def gallery():
-    return render_template('gallery.html')
+    return render_template('gallery.html', local_files=local_files)
+
 
 @app.route('/wedding_info')
 def wedding_info():
-    return render_template('base.html')
+    return render_template('base.html', local_files=local_files)
+
 
 @app.route('/lodging_info')
 def lodging_info():
-    return render_template('base.html')
+    return render_template('base.html', local_files=local_files)
+
 
 if __name__ == '__main__':
     port = int(os.environ.get('PORT', 5000))
--- static/js/main.js
+++ static/js/main.js
@@ -1,6 +1,16 @@
 /* global $ */
 $(document).ready(function() {
-    $('.js-expand-location-information').on('click', function() {
-        $('.js-location-information').toggleClass('hidden');
-    });
+  $('.js-expand-location-information').on('click', function() {
+    $('.js-location-information').toggleClass('hidden');
+  });
+
+  $('.js-toggle-electronic-invitation-form').on('click', function() {
+    $('.js-electronic-invitation-form').toggleClass('hidden');
+    $('.js-regrets-form').addClass('hidden');
+  });
+
+  $('.js-toggle-regrets-form').on('click', function() {
+    $('.js-regrets-form').toggleClass('hidden');
+    $('.js-electronic-invitation-form').addClass('hidden');
+  });
 });
